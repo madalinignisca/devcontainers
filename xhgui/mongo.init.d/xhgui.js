dbb284b1d00596bf320fccbd25b2910f2c9fb116
Madalin Ignisca <madalin.ignisca@multisafepay.com>
2021-08-23 11:41:08 +0200

    Refactor

--- xhgui/mongo.init.d/xhgui.js
+++
@@ -1,5 +0,0 @@
-db.results.ensureIndex( { 'meta.SERVER.REQUEST_TIME' : -1 } );
-db.results.ensureIndex( { 'profile.main().wt' : -1 } );
-db.results.ensureIndex( { 'profile.main().mu' : -1 } );
-db.results.ensureIndex( { 'profile.main().cpu' : -1 } );
-db.results.ensureIndex( { 'meta.url' : 1 } );
